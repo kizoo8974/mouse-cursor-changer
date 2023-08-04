5143b9c166ccae2abf57212228b8561c64d82e11
Joon Kim <kizoo8974@gamil.com>
2023-08-05 07:20:57 +0900

    cursor image function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+
+type Cursor = "cursor0" | "cursor1" | "cursor2" | "cursor3";
+
+const cursors:Cursor[] = ["cursor0", "cursor1", "cursor2", "cursor3"]
 
 function App() {
+  const [selectedCursor, setSelectedCursor] = useState<Cursor>("cursor0");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{
+        fontSize:"24px"
+      }}>
+        Press button and change mouse cursor.
+      </div>
+      <div style={{
+        marginTop:"16px",
+        display: "flex",
+        gap:"20px",
+        flexWrap:"wrap"
+
+      }}>
+        hello
+        {/*button list*/}
+      {
+        cursors.map(cursor => <Button onClick={()=>setSelectedCursor(cursor)} 
+        selected={selectedCursor === cursor} name={cursor} key={cursor}/>)
+      
+      }
+      </div>
+    </>
   );
 }
 
